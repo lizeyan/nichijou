f0f9b6a1724cfcbaae845412b09a551222714538
Zeyan Li <zy-li18@mails.tsinghua.edu.cn>
2020-09-18 18:03:38 +0800

    update

--- cp38_gpu/build-docker.sh
+++ cp38_gpu/build-docker.sh
@@ -5,5 +5,5 @@ docker_exec=docker
 tag=lizytalk/nichijou:cp38_gpu
 CONTEXT=$(realpath $(dirname "$0"))
 sudo ${docker_exec} pull ${tag}
-sudo ${docker_exec} build ${CONTEXT} ${BUILD_ARGS} -t ${tag}
+sudo ${docker_exec} build ${CONTEXT} ${BUILD_ARGS} -t ${tag} --network host
 sudo ${docker_exec} push ${tag}
